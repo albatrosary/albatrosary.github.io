a605f7fac3192a3a80cb2229303b835d32f3a20b
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-03-05 15:36:06 +0900

    ポリマー削除

--- sw.js
+++ sw.js
@@ -24,7 +24,9 @@ console.log('sw');
 
 // example usage:
 self.addEventListener('install', function(event) {
+  
   console.log('install');
+  
   event.waitUntil(
     caches.open('demo-cache').then(function(cache) {
       return cache.put('/', new Response("From the cache!"));
@@ -33,7 +35,9 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  
   console.log('fetch');
+
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || new Response("Nothing in the cache for this request");
