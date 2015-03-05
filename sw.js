c081772cd3bf70b323097792015dc73eb99824c2
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-03-05 14:07:30 +0900

    キャッシュさせたいファイルを配置

---
+++ sw.js
@@ -0,0 +1,17 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // インストール処理
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
