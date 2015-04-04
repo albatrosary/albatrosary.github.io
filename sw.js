4044d938c8a880b9091765ee05d0948be40a0b8c
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-04-04 16:25:28 +0900

    てすと

--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
 console.log('sw.js');
 
 self.onfetch = function(event) {
-  console.log('self.onfetch');
+  console.log('self.onfetch', event);
 }
+
+self.addEventListener('push', function(event) {
+
+  console.log('Received a push message', event);
+
+  var title = 'Yay a message.';  
+  var body = 'We have received a push message.';  
+  var icon = '/images/icon-192x192.png';  
+  var tag = 'simple-push-demo-notification-tag';
+
+  event.waitUntil(  
+    self.registration.showNotification(title, {  
+      body: body,  
+      icon: icon,  
+      tag: tag  
+    })  
+  );  
+});
