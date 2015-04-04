ebf6e78bb54e86396b14b88cb87233bc33440625
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-04-04 12:53:26 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
 console.log('sw');
 
 
-=======
->>>>>>> 7b404138d5590aa76edf998e3d8869821bc9f224
 self.onfetch = function(event) {
   
   console.log('fetch');
 
-<<<<<<< HEAD
 };
-=======
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || new Response("Nothing in the cache for this request");
-    })
-  );
-});
->>>>>>> 7b404138d5590aa76edf998e3d8869821bc9f224
