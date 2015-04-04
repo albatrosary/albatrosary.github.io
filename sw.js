e07d786d551c60aa8406fe6873392e698caddb93
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-04-04 12:52:00 +0900

    単純なモジュールに変更

--- sw.js
+++ sw.js
@@ -1,24 +1,8 @@
 console.log('sw');
-// example usage:
-// self.oninstall = function(event) {
-  
-//   console.log('install');
-  
-//   event.waitUntil(
-//     caches.open('demo-cache').then(function(cache) {
-//     console.log('install');
-//       return cache.put('/', new Response("From the cache!"));
-//     })
-//   );
-// });
+
 
 self.onfetch = function(event) {
   
   console.log('fetch');
 
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || new Response("Nothing in the cache for this request");
-    })
-  );
-});
+};
