7b404138d5590aa76edf998e3d8869821bc9f224
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-04-04 12:48:44 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,3 @@
-console.log('sw');
-// example usage:
-// self.oninstall = function(event) {
-  
-//   console.log('install');
-  
-//   event.waitUntil(
-//     caches.open('demo-cache').then(function(cache) {
-//     console.log('install');
-//       return cache.put('/', new Response("From the cache!"));
-//     })
-//   );
-// });
-
 self.onfetch = function(event) {
   
   console.log('fetch');
